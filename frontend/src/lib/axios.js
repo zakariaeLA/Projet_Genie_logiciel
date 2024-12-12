b04404fb39232a6ef22af0cd6438d884cb954cfe
zineb <zineb.baqili@enim.ac.ma>
2024-12-12 17:28:43 +0100

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5001/api" ,
+  baseURL: "http://localhost:7000/api/clubs/67508266533eb41d8194ded0/clubs",
   withCredentials: true,
 });
